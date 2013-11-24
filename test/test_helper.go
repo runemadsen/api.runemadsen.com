f48fddb7296fa67d1bec11dca686d4979e0c3a66
Rune Skjoldborg Madsen <rune@runemadsen.com>
2013-11-24 00:45:38 -0500

    working!

---
+++ test/test_helper.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+  "github.com/codegangsta/martini"
+  "net/http"
+  "net/http/httptest"
+  "reflect"
+  "testing"
+)
+
+// use this for initing stuff in the test
+//func init() {
+//   initDb()
+//}
+
+// Test Helpers
+// -----------------------------------------------------------------------
+
+func expect(t *testing.T, a interface{}, b interface{}) {
+  if a != b {
+    t.Errorf("Expected %v (type %v) - Got %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
+  }
+}
+
+func refute(t *testing.T, a interface{}, b interface{}) {
+  if a == b {
+    t.Errorf("Did not expect %v (type %v) - Got %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
+  }
+}
+
+func testRequest(method string, route string, handler martini.Handler) *httptest.ResponseRecorder {
+  m := martini.Classic()
+  m.Get("/", handler)
+  request, _ := http.NewRequest(method, route, nil)
+  response := httptest.NewRecorder()
+  m.ServeHTTP(response, request)
+  return response
+}
